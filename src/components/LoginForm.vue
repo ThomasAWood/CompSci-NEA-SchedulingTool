b074e3066b84ea00aed8338774201777afc90ed3
Thomas Wood <33393308+ThomasAWood@users.noreply.github.com>
2020-06-29 13:27:46 +0100

    Started adding login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class='container'>
+        <h1>Login</h1>
+        <form>
+            <div class="form-group">
+                <label for="email">Email</label><br>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Please enter your email...">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label><br>
+                <input type="password" class="form-control" name="password" id="password" placeholder="Please enter your password...">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+                
+                
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginForm'
+}
+</script>
+
+<style scoped>
+.container {
+    text-align: left;
+}
+</style>
