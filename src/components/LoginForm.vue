600ddf172a31978f22a974a216e5b9fe74922663
Thomas Wood <33393308+ThomasAWood@users.noreply.github.com>
2020-06-29 16:02:49 +0100

    Added styling and layout to the login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,20 +1,18 @@
 <template>
     <div class='container'>
-        <h1>Login</h1>
-        <form>
-            <div class="form-group">
-                <label for="email">Email</label><br>
+        <h1 class="text-center my-4">Login</h1>
+        <form action="" method=''>
+            <div class="form-group col-md-4 mx-auto my-3">
+                <label for="email">Email</label>
                 <input type="email" class="form-control" name="email" id="email" placeholder="Please enter your email...">
             </div>
-            <div class="form-group">
-                <label for="password">Password</label><br>
+            <div class="form-group col-md-4 mx-auto my-3">
+                <label for="password">Password</label>
                 <input type="password" class="form-control" name="password" id="password" placeholder="Please enter your password...">
             </div>
-            <div class="form-group">
+            <div class="form-group col-md-4 mx-auto text-center mt-4">
                 <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-                
-                
+            </div>  
         </form>
     </div>
 </template>
@@ -25,8 +23,6 @@ export default {
 }
 </script>
 
-<style scoped>
-.container {
-    text-align: left;
 }
+<style scoped>
 </style>
